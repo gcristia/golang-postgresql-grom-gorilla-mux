6b09cc8cb22bcbacfd474051185e45b6948cdc86
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2022-07-15 13:04:49 -0400

    Add GORM

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"github.com/gcristia/golang-postgresql-grom-gorilla-mux/db"
 	"github.com/gcristia/golang-postgresql-grom-gorilla-mux/routes"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
+
+	db.DBConnection()
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
