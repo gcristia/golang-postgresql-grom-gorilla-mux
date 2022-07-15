1d6aedc0bca976fe364e5027954b9006f4388f27
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2022-07-15 10:12:46 -0400

    Update Routes

--- routes/index.routes.go
+++ routes/index.routes.go
@@ -3,7 +3,7 @@ package routes
 import "net/http"
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("!!!"))
+	_, err := w.Write([]byte("Home !!"))
 	if err != nil {
 		return
 	}
